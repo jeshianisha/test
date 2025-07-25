a7d5ad3531bd8e5f9c06923bf80d3a4173e19f68
jeshianisha <jeshianisha14@gmail.com>
2025-07-25 20:03:07 +0530

    Add files via upload

---
+++ E 2.py
@@ -0,0 +1,39 @@
+#Arithmetic
+
+a=10
+b=5
+
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+modulo=a%b
+exponentiation=a**b
+floor_division=a//b
+
+print("Addition:",addition)
+print("Subtraction:",subtraction)
+print("Multiplication:",multiplication)
+print("Division:",division)
+print("Modulo:",modulo)
+print("Exponentiation:",exponentiation)
+print("Floor Division:",floor_division)
+
+#comparision Operatrs
+
+print("a>b:",a>b)
+print("a<b:",a<b)
+print("a>=b:",a>=b)
+print("a<=b:",a<=b)
+print("a==b:",a==b)
+print("a!=b:",a!=b)
+
+#logical Operators
+
+is_true=True
+is_false=False
+
+print("is_true and is_false:",is_true and is_false)
+print("is_true or is_false:",is_true or is_false)
+print("not is_true:",not is_true)
+
